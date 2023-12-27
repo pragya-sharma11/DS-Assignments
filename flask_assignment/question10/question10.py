beeb9ff9ca136850e000cc055fac48ac52ca8c2d
Pragya Sharma <pragya8528@gmail.com>
2023-12-27 16:23:05 +0530

    sorting assignment

--- flask_assignment/question10/question10.py
+++ flask_assignment/question10/question10.py
@@ -3,6 +3,8 @@
 from flask import * 
 app = Flask(__name__)
 
+
+
 # the below statement allows to Execute Code When the File Runs as a Script, 
 # but Not When It's Imported as a Module.
 if __name__ == '__main__':
